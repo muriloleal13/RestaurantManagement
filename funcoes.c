37bdc4d2d8aea22476ecd9d3686ea332da287510
muriloleal13 <mukaleal@gmail.com>
2019-11-05 12:50:02 -0300

    commit

--- funcoes.c
+++ funcoes.c
@@ -1,47 +1,101 @@
-Mesas** criaMesas(){
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <string.h>
+#include "funcoes.h"
+
+void criaMesas(){
 	return NULL;
 }
 
 //MESAS
-Mesas** insereMesas(Mesas** listaMesas, int row, int col){
+void preencheMesas(Mesas** matrizMesas, int row, int col){
 
-	listaMesas = (Mesas**) malloc(sizeof(Mesas*) * row);
+	int cont = 1;
+
 	for(int i = 0; i < row; i++){
-		listaMesas = (Mesas*) malloc(sizeof(Mesas) * col);
+		for(int j = 0; j < col; j++){
+			matrizMesas[i][j].nro = cont;
+			matrizMesas[i][j].livre = true;
+			cont++;
+		}
 	}
-
-	return listaMesas;
 }
 
-Mesas** cadastraMesas(Mesas** listaMesas){
+void imprimeMesas(Mesas** matrizMesas, int row, int col){
 
-	int row, col;
+	int cont = 1;
 
-	printf("=== CADASTRA MESAS ===\n");
+	if(row == 0 || col == 0){
+		for(int i = 0; i < row; i++){
+			for(int j = 0; j < col; j++){
+				char status[10];
+				if(matrizMesas[i][j].livre)
+					strcpy(status, "Livre");	
+				else
+					strcpy(status, "Ocupado");
 
-	printf("Digite o numero de linhas de mesas: ");
-	scanf("%d", &row);
-	fflush(stdin);
-	printf("Digite o numero de colunas de mesas: ");
-	scanf("%d", &col);
-	fflush(stdin);
-
-	listaMesas = insereMesas(listaMesas, row, col);
-
-	return listaMesas;
+				printf("%d\nMesa %d\nStatus: %s\n =X=X=X=X=X=X=X=X=X=", cont, matrizMesas[i][j].nro, status);
+				cont++;
+			}
+		}
+	}else{
+		printf("Sem mesas cadastradas\n");
+	}
 }
 
 void menu(){
 
-	Mesas** listaMesas = criaMesas();
+	Mesas **matrizMesas = NULL;
+	int op;
+	int opCliente;
+	int row = 0, col = 0;
 
-	int op = opMenu();
+	printf("=== MENU ===\n");
+
+	printf("1 - Mesas\n2 - Estacionamento\n3 - Sair\nOp: ");
+	scanf("%d", &op);
 	fflush(stdin);
 
 	while(op != 3){
 		switch(op){
 			case 1:
-				listaClientes = menuMesas(listaMesas);
+				printf("=== Menu Mesas === \n");
+				printf("1 - Cadastrar Mesas\n2 - Imprime Mesas\n3- Voltar ao Menu\nOp: ");
+				scanf("%d", &opCliente);
+				fflush(stdin);
+				while(opCliente != 3){
+					switch(opCliente){
+						case 1:
+							printf("=== CADASTRA MESAS ===\n");
+							printf("Digite o numero de linhas de mesas: ");
+							scanf("%d", &row);
+							fflush(stdin);
+							printf("Digite o numero de colunas de mesas: ");
+							scanf("%d", &col);
+							fflush(stdin);
+
+						    matrizMesas = (Mesas **)malloc(row *(sizeof(struct mesa*)));
+						    for (int i = 0; i < row; i++)
+						      	matrizMesas[i] = (Mesas *)malloc(col *(sizeof(struct mesa)));
+						     
+						    preencheMesas(matrizMesas, row, col);
+							break;
+						case 2:
+							imprimeMesas(matrizMesas, row, col);
+							break;
+						case 3:
+							printf("Voltando ao menu.\n");
+							break;
+						default:
+							printf("Operacao invalida.\n");
+							break;
+					}
+					printf("1 - Cadastrar Mesa\n2 - Imprime Mesas\n3 - Voltar ao Menu\nOp: ");
+					scanf("%d", &opCliente);
+					fflush(stdin);
+				}
 				break;
 			case 2:
 				break;
@@ -53,54 +107,9 @@ void menu(){
 				break;
 		}
 		system("cls");
-		op = opMenu();
+		printf("1 - Mesas\n2 - Estacionamento\n3 - Sair\nOp: ");
+		scanf("%d", &op);
+		fflush(stdin);
 	}
-
-int opMenu(){
-
-	int op;
-
-	printf("=== MENU ===\n");
-
-	printf("1 - Mesas\n2 - Estacionamento\n3 - Sair\nOp: ");
-	scanf("%d", &op);
-	fflush(stdin);
-
-	return op;
-}
-
-int opMenuTipos(char str[]){
-
-	int op;
-
-	printf("=== Menu %s === \n", str);
-
-	printf("1 - Cadastrar %s\n2 - Voltar ao Menu\nOp: ", str, str, str);
-	scanf("%d", &op);
-	fflush(stdin);
-
-	return op;
-}
-
-Mesas** menuMesas(Mesas** listaMesas){
-
-	int opCliente = opMenuTipos("Mesas");
-	
-	while(opCliente != 4){
-		switch(opCliente){
-			case 1:
-				listaMesas = cadastraMesas(listaMesas);
-				break;
-			case 2:
-				printf("Voltando ao menu.\n");
-				break;
-			default:
-				printf("Operacao invalida.\n");
-				break;
-		}
-		opCliente = opMenuTipos("Clientes");
-	}
-	return listaClientes;
-}
 
 }
--- funcoes.h
+++ funcoes.h
@@ -1,19 +1,11 @@
-typedef struct mesas{
+typedef struct mesa{
 	int nro;
 	bool livre;
-} Mesa;
-
-typedef struct lista_mesas{
-	Conta conta;
-	struct lista_mesas* prox;
 } Mesas;
 
 //MESAS
-Mesas** criaMesas();
-Mesas** insereMesas (Mesas* listaMesas, int row, int col);
+void preencheMesas(Mesas** matrizMesas, int row, int col);
+void imprimeMesas(Mesas** matrizMesas, int row, int col);
 
 //Menu
 void menu();
-int opMenu();
-int opMenuTipos(char str[])
-Mesas** menuMesas(Mesas** listaMesas);
